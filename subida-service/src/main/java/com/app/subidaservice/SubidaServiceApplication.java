b32d0fce7fd01ca6cf756224319489761cd782ce
LeoVergaraS <leo.vergara@usach.cl>
2022-11-23 15:24:07 -0300

    a

--- subida-service/src/main/java/com/app/subidaservice/SubidaServiceApplication.java
+++ subida-service/src/main/java/com/app/subidaservice/SubidaServiceApplication.java
@@ -5,22 +5,33 @@ import javax.annotation.Resource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import com.app.subidaservice.service.FileUploadService;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class SubidaServiceApplication implements CommandLineRunner{
-
-	@Resource
-	FileUploadService fileUploadService;
+public class SubidaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubidaServiceApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		fileUploadService.deleteDirectory();
-		fileUploadService.init();
-	}	
 }
+
+// @SpringBootApplication
+// public class SubidaServiceApplication implements CommandLineRunner{
+
+// @Resource
+// FileUploadService fileUploadService;
+
+// public static void main(String[] args) {
+// SpringApplication.run(SubidaServiceApplication.class, args);
+// }
+
+// @Override
+// public void run(String... args) throws Exception {
+// fileUploadService.deleteDirectory();
+// fileUploadService.init();
+// }
+// }
